0dc70fe2bd822ca0fcf72912aeadcec38b9d319f
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2024-02-12 14:43:03 +0600

    file naming convensions

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,5 @@
-import { Controller, Module, Get } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
-@Controller()
-class AppController{
-    @Get()
-    getRootRoute(){
-        return "Hi There!";
-    }
-}
-
-@Module({
-    controllers: [AppController]
-})
-
-class AppModule{}
+import { AppModule } from "./app.module";
 
 async function bootstrap(){
     const app = await NestFactory.create(AppModule);
